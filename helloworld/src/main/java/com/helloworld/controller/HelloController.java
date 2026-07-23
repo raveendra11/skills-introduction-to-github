7303f9ba8fa2d1eed37c439f67e02fabfff3cfa7
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2026-07-23 22:52:03 +0000

    Added helloworld to practice on git cmds.

---
+++ helloworld/src/main/java/com/helloworld/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    
+    @GetMapping("/messages")
+    public String messages() {
+    	return "Hello, world!";
+    }
+    
+}
